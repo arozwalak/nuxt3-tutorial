31b949dcea76f4e1cd9024c821063edd8880acbf
Artur <artur.rozwalak@gmail.com>
2024-01-25 17:24:42 +0000

    Product details

---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, in provident maxime harum tenetur asperiores sint, illum iure libero error obcaecati fugit officiis dolores. Corporis unde delectus ullam quis accusantium?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, in provident maxime harum tenetur asperiores sint, illum iure libero error obcaecati fugit officiis dolores. Corporis unde delectus ullam quis accusantium?</p>
+    </div>
+</template>
+
+
+<style scoped>
+    h2 {
+        margin-bottom: 20x;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ nuxt-dojo/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, in provident maxime harum tenetur asperiores sint, illum iure libero error obcaecati fugit officiis dolores. Corporis unde delectus ullam quis accusantium?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, in provident maxime harum tenetur asperiores sint, illum iure libero error obcaecati fugit officiis dolores. Corporis unde delectus ullam quis accusantium?</p>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
